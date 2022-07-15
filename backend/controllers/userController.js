efeb4c69d882ecef0b9aeda8aa7e3f176baa71f0
Robbie Prokop <robbieprokop@gmail.com>
2022-07-15 15:51:10 -0600

    installed bcrypt and JWT, created user routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,21 @@
+//@desc   Create User
+//@route  POST /api/users
+//@access   Public
+const registerUser = (req, res) => {
+  res.json({ message: "Register User" });
+};
+
+//@desc   Authenticate User
+//@route  POST /api/users/login
+//@access   Public
+const loginUser = (req, res) => {
+  res.json({ message: "Login User" });
+};
+
+//@desc   Get User Data
+//@route  GET /api/users/me
+//@access   Public
+const getMe = (req, res) => {
+  res.json({ message: "User Data Displayed" });
+};
+module.exports = { registerUser, loginUser, getMe };
