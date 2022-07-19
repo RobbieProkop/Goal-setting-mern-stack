43ab4a434da4876557733dab94bb0229dba0a2b8
Robbie Prokop <robbieprokop@gmail.com>
2022-07-18 21:52:41 -0600

    fixed error of not submitting goal (missed curly braces)

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
+import { createGoal } from "../features/goals/goalSlice";
 
 const GoalForm = () => {
   const [text, setText] = useState("");
+  const dispatch = useDispatch();
+
   const onSubmit = (e) => {
     e.preventDefault();
+    dispatch(createGoal({ text }));
+    setText("");
   };
   return (
     <section className="form">
