dec6abb9304cddb36321f591d053f4894237c2b2
Robbie Prokop <robbieprokop@gmail.com>
2022-07-18 18:00:06 -0600

    start building GoalForm

---
+++ frontend/src/features/goals/goalSlice.js
@@ -0,0 +1,24 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import goalService from "./goalService";
+
+const initialState = {
+  goals: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+export const goalSlice = createSlice({
+  name: "goal",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  // extraReducers: (builder) => {
+  //   builder.addCase();
+  // },
+});
+
+export const { reset } = goalSlice.actions;
+export default goalSlice.reducer;
